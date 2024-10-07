93e1b07c6a495854f2f7610510039746c276d1b1
Naveen Kendyala <subscribeken@gmail.com>
2024-10-06 23:03:52 -0400

    Updated challenge

---
+++ src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,15 @@
+package org.acme;
+
+import java.time.LocalDate;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/api")
+public class GreetingResource {
+
+//TO DO :: Write the challenge-02 method
+
+}
